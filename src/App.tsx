2990039a73067f9d78d36e8026aba3a90801ce78
Kirill190611 <k.igorevitch88@yandex.ru>
2025-01-27 22:00:12 +0300

    1. Added case when tasks array is empty; 2. Added Button component; 3. Deleted date from props;

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,10 @@ export const App = () => {
         { id: 3, title: 'ReactJS', isDone: false },
     ]
 
-    const tasks2: Task[] = [
-        { id: 1, title: 'Hello world', isDone: true },
-        { id: 2, title: 'I am Happy', isDone: false },
-        { id: 3, title: 'Yo', isDone: false },
-    ]
 
     return (
         <div className="app">
-            <TodolistItem title='What to learn' tasks={tasks1} date='27.01.2025'/>
-            <TodolistItem title='Songs' tasks={tasks2}/>
+            <TodolistItem title='What to learn' tasks={tasks1} />
         </div>
     )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,39 +1,44 @@
 import {Task} from "./App.tsx";
+import {Button} from "./Button.tsx";
 
 export type TodolistItemProps = {
     title: string
     tasks: Task[]
-    date?: string
 }
 
 export const TodolistItem = (props: TodolistItemProps) => {
-    const {title, tasks, date} = props
+    const {title, tasks} = props
 
     return (
         <div>
             <h3>{title}</h3>
             <div>
                 <input/>
-                <button>+</button>
+                <Button title='+'/>
             </div>
-            <ul>
-                {
-                    tasks.map(task => {
-                        return (
-                            <li key={task.id}>
-                                <input type="checkbox" checked={task.isDone}/>
-                                <span>{task.title}</span>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            {
+                tasks.length === 0 ? (
+                    <p>There is no any data</p>
+                ) : (
+                    <ul>
+                        {
+                            tasks.map(task => {
+                                return (
+                                    <li key={task.id}>
+                                        <input type="checkbox" checked={task.isDone}/>
+                                        <span>{task.title}</span>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                )
+            }
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <Button title='All'/>
+                <Button title='Active'/>
+                <Button title='Completed'/>
             </div>
-            <div>{date}</div>
         </div>
     )
 }
