98c01c9deb9117bd17877a975b49ef8fb6708dca
Kirill190611 <k.igorevitch88@yandex.ru>
2025-02-27 20:27:51 +0300

    1. Added set todolist AC in todolist slice for receiving data from server; 2. Some code refactoring;

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,10 +2,21 @@ import Grid from '@mui/material/Grid2'
 import Paper from '@mui/material/Paper'
 import { TodolistItem } from '@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx'
 import { selectTodolists } from '@/features/todolists/model/todolists-selectors.ts'
-import { useAppSelector } from '@/common/hooks'
+import { useAppDispatch, useAppSelector } from '@/common/hooks'
+import { useEffect } from 'react'
+import { todolistApi } from '@/features/todolists/api/todolistApi.ts'
+import { setTodolistAC } from '@/features/todolists/model/todolist-slice.ts'
 
 export const Todolists = () => {
   const todolists = useAppSelector(selectTodolists)
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    todolistApi.getTodolist().then((res) => {
+      dispatch(setTodolistAC({ todolists: res.data }))
+    })
+  }, [])
 
   return (
     <>
