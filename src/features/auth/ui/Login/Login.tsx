27e0d3fa450694d3287c51b9f8263bc949af2991
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-12 21:40:47 +0300

    1. Added handleSubmit;

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -9,7 +9,7 @@ import FormGroup from '@mui/material/FormGroup'
 import FormLabel from '@mui/material/FormLabel'
 import Grid from '@mui/material/Grid2'
 import TextField from '@mui/material/TextField'
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 type Inputs = {
   email: string
@@ -29,38 +29,44 @@ export const Login = () => {
     formState: { errors },
   } = useForm<Inputs>({ defaultValues: { email: '', password: '', rememberMe: false } })
 
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
+
   return (
     <Grid container justifyContent={'center'}>
-      <FormControl>
-        <FormLabel>
-          <p>
-            To login get registered
-            <a
-              style={{ color: theme.palette.primary.main, marginLeft: '5px' }}
-              href='https://social-network.samuraijs.com'
-              target='_blank'
-              rel='noreferrer'
-            >
-              here
-            </a>
-          </p>
-          <p>or use common test account credentials:</p>
-          <p>
-            <b>Email:</b> free@samuraijs.com
-          </p>
-          <p>
-            <b>Password:</b> free
-          </p>
-        </FormLabel>
-        <FormGroup>
-          <TextField label='Email' margin='normal' {...register('email')} />
-          <TextField type='password' label='Password' margin='normal' {...register('password')} />
-          <FormControlLabel label='Remember me' control={<Checkbox {...register('rememberMe')} />} />
-          <Button type='submit' variant='contained' color='primary'>
-            Login
-          </Button>
-        </FormGroup>
-      </FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: theme.palette.primary.main, marginLeft: '5px' }}
+                href='https://social-network.samuraijs.com'
+                target='_blank'
+                rel='noreferrer'
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
+          <FormGroup>
+            <TextField label='Email' margin='normal' {...register('email')} />
+            <TextField type='password' label='Password' margin='normal' {...register('password')} />
+            <FormControlLabel label='Remember me' control={<Checkbox {...register('rememberMe')} />} />
+            <Button type='submit' variant='contained' color='primary'>
+              Login
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
     </Grid>
   )
 }
