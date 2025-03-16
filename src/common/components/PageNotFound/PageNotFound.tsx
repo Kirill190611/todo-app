853babca24207638ce842e3305f0e15e2b3f6cbd
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-16 15:39:37 +0300

    1. Added button link with MUI in page not found component with redirect to main page; 2. Some code refactoring;

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,16 @@
 import styles from './PageNotFound.module.css'
+import Button from '@mui/material/Button'
+import { Link } from 'react-router'
+import { Path } from '@/common/routing'
 
 export const PageNotFound = () => {
   return (
-    <>
+    <div className={styles.container}>
       <h1 className={styles.title}>404</h1>
       <h2 className={styles.subtitle}>Oops... Something wrong! Page not found</h2>
-    </>
+      <Button className={styles.button} component={Link} to={Path.Main} variant={'contained'}>
+        Вернуться на главную
+      </Button>
+    </div>
   )
 }
