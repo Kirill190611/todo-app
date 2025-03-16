2b9412e33b38bc8cf9bd48b97b8797a70228abc1
Kirill190611 <k.igorevitch88@yandex.ru>
2025-03-16 17:12:50 +0300

    1. Added login TC; 2. Some code refactoring;

--- src/features/auth/model/__tests__/auth-slice.ts
+++ src/features/auth/model/__tests__/auth-slice.ts
@@ -1,6 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { LoginArgs } from '@/features/auth/api/authApi.types.ts'
+import { createAppSlice, handleServerNetworkError } from '@/common/utils'
+import { setAppStatusAC } from '@/app/app-slice.ts'
+import { authApi } from '@/features/auth/api/authApi.ts'
+import { ResultCode } from '@/common/enums'
 
-export const authSlice = createSlice({
+export const authSlice = createAppSlice({
   name: 'auth',
   initialState: {
     isLoggedIn: false,
@@ -16,6 +20,20 @@ export const authSlice = createSlice({
     }),
     setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
       state.isInitialized = action.payload.isInitialized
+    }),
+    loginTC: create.asyncThunk(async (data: LoginArgs, { dispatch, rejectWithValue }) => {
+      try {
+        dispatch(setAppStatusAC({ status: 'loading' }))
+        const res = await authApi.login(data)
+        if (res.data.resultCode === ResultCode.Success) {
+          dispatch(setAppStatusAC({ status: 'succeeded' }))
+          dispatch(setIsLoggedIn({ isLoggedIn: true }))
+          localStorage.setItem('sn-token', res.data.data.token)
+        }
+      } catch (error: any) {
+        handleServerNetworkError(error, dispatch)
+        return rejectWithValue(null)
+      }
     }),
   }),
 })
