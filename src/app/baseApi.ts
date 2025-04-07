0afc8f5fb48089f0845c57951946b3fd2ebe50fa
Kirill190611 <k.igorevitch88@yandex.ru>
2025-04-07 22:02:02 +0300

    1. Added fetch error handler in base api; 2. Some code refactoring;

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,14 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setAppErrorAC } from '@/app/app-slice.ts'
 
 export const baseApi = createApi({
   reducerPath: 'todolistsApi',
   tagTypes: ['Todolist', 'Task'],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set('API-KEY', `${import.meta.env.VITE_API_KEY}`)
-      headers.set('Authorization', `Bearer ${localStorage.getItem('sn-token')}`)
-    },
-  }),
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      prepareHeaders: (headers) => {
+        headers.set('API-KEY', `${import.meta.env.VITE_API_KEY}`)
+        headers.set('Authorization', `Bearer ${localStorage.getItem('sn-token')}`)
+      },
+    })(args, api, extraOptions)
+
+    if (result.error) {
+      if (result.error.status === 'FETCH_ERROR') {
+        api.dispatch(setAppErrorAC({ error: result.error.error }))
+      }
+    }
+    return result
+  },
   endpoints: () => ({}),
 })
