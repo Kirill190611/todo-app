dedef68e17cb87e91941af67e435fcd2c1a31a1c
Kirill190611 <k.igorevitch88@yandex.ru>
2025-04-19 21:12:01 +0300

    1. Added optimistic update logic for remove todo on API level; 2. Some code refactoring;

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -3,11 +3,9 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import IconButton from '@mui/material/IconButton'
 import styles from './TodolistTitle.module.css'
 import {
-  todolistsApi,
   useRemoveTodolistMutation,
   useUpdateTodolistTitleMutation,
 } from '@/features/todolists/api/todolistsApi.ts'
-import { useAppDispatch } from '@/common/hooks'
 import { DomainTodolist } from '@/features/todolists/lib/types'
 
 type Props = {
@@ -17,52 +15,11 @@ type Props = {
 export const TodolistTitle = ({ todolist }: Props) => {
   const { id, title } = todolist
 
-  const dispatch = useAppDispatch()
-
   const [removeTodolist] = useRemoveTodolistMutation()
   const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
-  // const changeTodolistStatus = (entityStatus: RequestStatus) => {
-  //   dispatch(
-  //     todolistsApi.util.updateQueryData(
-  //       'getTodolists',
-  //       undefined,
-  //       (todolists: DomainTodolist[]) => {
-  //         const todolist = todolists.find((todolist) => todolist.id === id)
-  //         if (todolist) {
-  //           todolist.entityStatus = entityStatus
-  //         }
-  //       }
-  //     )
-  //   )
-  // }
-
-  // const deleteTodolist = () => {
-  //   changeTodolistStatus('loading')
-  //   removeTodolist(id)
-  //     .unwrap()
-  //     .catch(() => {
-  //       changeTodolistStatus('idle')
-  //     })
-  // }
-
-  const deleteTodolist = async () => {
-    const patchResult = dispatch(
-      todolistsApi.util.updateQueryData('getTodolists', undefined, (state) => {
-        const index = state.findIndex((todolist) => todolist.id === id)
-        if (index !== -1) {
-          state.splice(index, 1)
-        }
-      })
-    )
-    try {
-      const res = await removeTodolist(id)
-      if (res.error) {
-        patchResult.undo()
-      }
-    } catch {
-      patchResult.undo()
-    }
+  const deleteTodolist = () => {
+    removeTodolist(id)
   }
 
   const changeTodolistTitle = (title: string) => {
